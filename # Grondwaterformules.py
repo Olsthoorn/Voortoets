9e434796a08d217c25af265a187526c610c7891e
Theo Olsthoorn <tolsthoorn@google.com>
2025-01-28 00:56:00 +0100

    De Laat's theory added partly.

---
+++ # Grondwaterformules.py
@@ -0,0 +1,6 @@
+# Grondwaterformules.nl: de verlaging van een kleine onttrekking is maximaal bij een bepaalde kD
+
+
+
+
+$$\siigma = \frac{2 \pi}{Q} s = \frac{1}{T} K_0\left(\frac r \lambda\right)$$
